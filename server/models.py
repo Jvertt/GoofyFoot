8b5bebe3b907901e27909735fcab316fefbc4fb7
Justin Verthein <Justinmackay88@hotmail.com>
2023-07-24 14:24:51 -0700

    set up tables

--- server/models.py
+++ server/models.py
@@ -1,7 +1,39 @@
 from sqlalchemy_serializer import SerializerMixin
-
 from config import db
+from flask_sqlalchemy import SQLAlchemy
 
-# Models go here!
+db = SQLAlchemy()
 
+class User(db.Model):
+    __tablename__ = "users"
 
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+
+    products = db.relationship('Product', backref='user', lazy=True)
+    reviews = db.relationship('Review', backref='user', lazy=True)
+
+class Product(db.Model):
+    __tablename__ = "products"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    reviews = db.relationship('Review', secondary='product_review', backref='products')
+
+class Review(db.Model):
+    __tablename__ = "reviews"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(500), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    products = db.relationship('Product', secondary='product_review', backref='reviews')
+
+product_review = db.Table('product_review',
+    db.Column('product_id', db.Integer, db.ForeignKey('products.id'), primary_key=True),
+    db.Column('review_id', db.Integer, db.ForeignKey('reviews.id'), primary_key=True)
+)
