96c281a576eab3dfdb149b284d41bf63d0282651
Justin Verthein <Justinmackay88@hotmail.com>
2023-07-24 14:20:38 -0700

    set up db and project template

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  // Code goes here!
+  hi
 }
 
 export default App;
--- server/models.py
+++ server/models.py
@@ -3,3 +3,5 @@ from sqlalchemy_serializer import SerializerMixin
 from config import db
 
 # Models go here!
+
+
