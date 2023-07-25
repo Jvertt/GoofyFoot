900fa3db7f3ec86346f26c885b4ad22af54c6bd5
Justin Verthein <Justinmackay88@hotmail.com>
2023-07-25 15:59:53 -0700

    added the ability for lessons to be appened to booking

--- client/src/components/LessonDetails.js
+++ client/src/components/LessonDetails.js
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const LessonDetails = ({ lessons }) => {
   const { id } = useParams();
   const lesson = lessons.find(lesson => lesson.id === parseInt(id));
 
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
   if (!lesson) {
-    return <div>Loading...</div>; // Handle the case when the lesson data is not available yet
+    return <div>Loading...</div>;
   }
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post('http://127.0.0.1:5555/bookings', {
+        name,
+        email,
+        lesson_id: lesson.id,
+      });
+
+      // handle response as needed
+      console.log(response.data);
+
+      // Reset the input fields after successful submission
+      setName('');
+      setEmail('');
+    } catch (error) {
+      // handle error as needed
+      console.error(error);
+    }
+  };
 
   return (
     <div className="lesson-details">
       <h2>{lesson.title}</h2>
       <p>{lesson.description}</p>
-      <form>
+      <form onSubmit={handleFormSubmit}>
         <label>Name:</label>
-        <input type="text" />
+        <input type="text" value={name} onChange={e => setName(e.target.value)} />
         <label>Email:</label>
-        <input type="email" />
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
         <button type="submit">Book Lesson</button>
       </form>
     </div>
