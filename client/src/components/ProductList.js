e11924ce1b5b675788e4154b7c18eafae54caecb
Justin Verthein <Justinmackay88@hotmail.com>
2023-07-24 17:14:58 -0700

    added fetch request to product list

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -2,11 +2,27 @@ import React, { useEffect, useState } from 'react';
 
 function ProductList (){
   const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    // TODO: Fetch products from the server and set them to state.
+    fetch('http://127.0.0.1:5555/products') // Replace with your actual server URL
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch products');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setProducts(data);
+      })
+      .catch(error => {
+        setError(error.message);
+      });
   }, []);
 
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
   return (
     <div className="product-list">
       {products.map(product => (
