6aad6d45283c21132a255ed2a8a6e1c7cae9367f
Justin Verthein <Justinmackay88@hotmail.com>
2023-07-28 19:32:37 -0700

    added redirect to home and popup ot manage boookings component

--- client/src/components/ManageBookings.js
+++ client/src/components/ManageBookings.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import '../index.css'
+import { useHistory } from 'react-router-dom';
+import '../index.css';
 
 function ManageBookings() {
+  const history = useHistory();
   const [bookings, setBookings] = useState([]);
   const [name, setName] = useState('');
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [bookingToCancel, setBookingToCancel] = useState(null);
 
   const fetchBookings = () => {
     axios.get('http://127.0.0.1:5555/bookings')
@@ -20,17 +24,26 @@ function ManageBookings() {
     fetchBookings();
   }, []);
 
-  function cancelBooking(id) {
-    axios.delete(`http://127.0.0.1:5555/bookings/${id}`)
+  const cancelBooking = (id) => {
+    const booking = bookings.find(booking => booking.id === id);
+    setBookingToCancel(booking);
+    setShowConfirmation(true);
+  };
+
+  const handleConfirmation = () => {
+    axios.delete(`http://127.0.0.1:5555/bookings/${bookingToCancel.id}`)
       .then(() => {
         // Fetch the bookings from the server again after the deletion.
         return axios.get('http://127.0.0.1:5555/bookings');
       })
       .then(response => {
         setBookings(response.data);
+        setShowConfirmation(false);
+        history.push('/');
       })
       .catch(error => {
         console.error(error);
+        setShowConfirmation(false);
       });
   };
 
@@ -46,6 +59,19 @@ function ManageBookings() {
           <button className="cancel-button" onClick={() => cancelBooking(booking.id)}>Cancel Booking</button>
         </div>
       ))}
+
+      {showConfirmation && bookingToCancel && (
+        <div className="confirmation-overlay">
+          <div className="confirmation-popup">
+            <h3>Confirm Booking Cancellation</h3>
+            <p>Please confirm the cancellation of the booking:</p>
+            <p>Name: {bookingToCancel.name}</p>
+            <p>Email: {bookingToCancel.email}</p>
+            <button onClick={handleConfirmation}>Confirm</button>
+            <button onClick={() => setShowConfirmation(false)}>Cancel</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
